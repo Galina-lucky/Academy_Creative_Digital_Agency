c127e41b9b2664d1c2f6348fb76f0c265de233c3
Galina-lucky <gala.nitch@gmail.com>
2021-01-20 16:39:44 +0200

    Adaptive 1,2,3 sections

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,19 @@
 ;(function(){
 
   $(document).ready(function() {
-    
+    let menu = $('.js-menu');
+    let menuBtn = $('.js-btn-toggle-menu');
+    let el = $('.js-btn-toggle-menu__item');
+
+    menuBtn.on('click', function() {
+      el.toggleClass('btn-toggle-menu__item--big');
+    });
+
+    $(document).click(function (e) {
+      if ( !menuBtn.is(e.target)  && !el.is(e.target)  && !menu.is(e.target) && el.hasClass('btn-toggle-menu__item--big')) {
+        el.removeClass('btn-toggle-menu__item--big');
+      };
+    });
   });
   
 })();
